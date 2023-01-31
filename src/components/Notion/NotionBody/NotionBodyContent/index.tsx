4fe53cb5ee8e16683ca89fdd02ca68ff698bf5b1
Ivan Varabyou <85835188+ivan-varabyou@users.noreply.github.com>
2023-01-31 23:07:52 +0300

    feat: add example content component

--- src/components/Notion/NotionBody/NotionBodyContent/index.tsx
+++ src/components/Notion/NotionBody/NotionBodyContent/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./NotionBodyContent.module.scss";
+import { NotionBodyContentRow } from "./NotionBodyContentRow";
 import { ReactComponent as EmojiSVG } from "../../../../assets/img/svg/emoji.svg";
 import { ReactComponent as CoverSVG } from "../../../../assets/img/svg/cover.svg";
 
@@ -13,38 +14,78 @@ export const NotionBodyContent = (): React.ReactElement => {
           className={styles.content__wrapper}
           style={{ width: `${100 / widthPageDivision}%` }}
         >
-          <div className={styles.content__emoji}>📗</div>
+          <div className={styles.content__header}>
+            <div className={styles.content__icon}>
+              <span className={styles.content__emoji}>📗</span>
+            </div>
+            <div className={styles.content__toolbar}>
+              <div
+                className={styles.content__toolbar_button + " button_control"}
+              >
+                <span className={styles.content__toolbar_svg}>
+                  <EmojiSVG />
+                </span>
+                <span className={styles.content__toolbar_text}>Add icon</span>
+              </div>
 
-          <div className={styles.content__toolbar}>
-            <div className={styles.content__toolbar_button}>
-              <span className={styles.content__toolbar_svg}>
-                <EmojiSVG />
-              </span>
-              <span className={styles.content__toolbar_text}>Add icon</span>
+              <div
+                className={styles.content__toolbar_button + " button_control"}
+              >
+                <span className={styles.content__toolbar_svg}>
+                  <CoverSVG />
+                </span>
+                <span className={styles.content__toolbar_text}>Add cover</span>
+              </div>
             </div>
 
-            <div className={styles.content__toolbar_button}>
-              <span className={styles.content__toolbar_svg}>
-                <CoverSVG />
-              </span>
-              <span className={styles.content__toolbar_text}>Add cover</span>
+            <div className={styles.content__title}>Object pages</div>
+            <div className={styles.content__comment}>
+              <div className={styles.content__comment_avatar}>
+                <img src="https://lh3.googleusercontent.com/a-/AFdZucrnvCnEsd0erWUTqf6_bmSJLRbWfPGvfHrSb5w1yg=s100" />
+              </div>
+              <div
+                className={styles.content__comment_input}
+                placeholder="Add a comment…"
+              >
+                Add a comment…
+              </div>
             </div>
           </div>
 
-          <div className={styles.content__title}>Object pages</div>
-          <div className={styles.content__comment}>
-            <div className={styles.content__comment_avatar}>
-              <img src="https://lh3.googleusercontent.com/a-/AFdZucrnvCnEsd0erWUTqf6_bmSJLRbWfPGvfHrSb5w1yg=s100" />
-            </div>
-            <div
-              className={styles.content__comment_input}
-              placeholder="Add a comment…"
-            >
-              Add a comment…
-            </div>
-          </div>
+          <NotionBodyContentRow
+            block="notion-text-block"
+            color="#f90000"
+            placeholder="Type '/' for commands"
+            content="👋 Welcome to my ninja notion clone!"
+          />
 
-          <div className={styles.content__row}></div>
+          <NotionBodyContentRow
+            block="notion-h1-block"
+            color=""
+            placeholder="Heading 1"
+            content="👋 Welcome to my ninja notion clone!"
+          />
+
+          <NotionBodyContentRow
+            block="notion-h2-block"
+            color=""
+            placeholder="Heading 2"
+            content="👋 Welcome to my ninja notion clone!"
+          />
+
+          <NotionBodyContentRow
+            block="notion-h3-block"
+            color=""
+            placeholder="Heading 3"
+            content="👋 Welcome to my ninja notion clone!"
+          />
+
+          <NotionBodyContentRow
+            block="notion-code-block"
+            color=""
+            placeholder="Code ..."
+            content="cover': { 'link': { 'url': 'https://url.com' }}"
+          />
         </div>
       </div>
     </>
