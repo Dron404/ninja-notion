5b7e6ed9f9c84107f1d9facef108ad7bf68f2b4c
Andrei Mashedo <ahdma@mail.ru>
2023-02-04 16:59:12 +0300

    refactor: remove .pritter.

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -8,31 +8,23 @@ function HomeHeader(props: {
 }) {
   const { setMobileMenuActive, mobileMenuActive } = props;
 
-  const HeaderBurgerIcon = (
-    <button
-      type="button"
-      className="burger-menu"
-      onClick={() => setMobileMenuActive(!mobileMenuActive)}
-    >
-      <BurgerIcon width="22px" height="22px" className="burger-icon" />
-    </button>
-  );
-
-  const HeaderRegistration = (
-    <div className="registration">
-      <div className="registration__log">Log In</div>
-      <button type="button" className="button button_header">
-        Try Notion Free
-      </button>
-    </div>
-  );
-
   return (
     <header className="header">
       <div className="header__wrapper">
         <HomeLogo />
-        {HeaderRegistration}
-        {HeaderBurgerIcon}
+        <div className="registration">
+          <div className="registration__log">Log In</div>
+          <button type="button" className="button button_header">
+            Try Notion Free
+          </button>
+        </div>
+        <button
+          type="button"
+          className="burger-menu"
+          onClick={() => setMobileMenuActive(!mobileMenuActive)}
+        >
+          <BurgerIcon width="22px" height="22px" className="burger-icon" />
+        </button>
       </div>
     </header>
   );
