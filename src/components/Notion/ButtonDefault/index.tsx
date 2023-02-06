f3faa488f1ed73b218b6231cace267ed81e5ed7a
Ivan Varabyou <85835188+ivan-varabyou@users.noreply.github.com>
2023-02-06 16:54:57 +0300

    feat: add default button

---
+++ src/components/Notion/ButtonDefault/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./ButtonDefault.module.scss";
+import { IButtonDefault } from "../../../types/interface";
+
+export const ButtonDefault: React.FC<IButtonDefault> = ({
+  text,
+  type = "default",
+  handle,
+}) => {
+  let style = styles.button__default;
+  if (type === "danger" || type === "warning" || type === "delete")
+    style = styles.button__warning;
+  if (type === "primary") style = styles.button__primary;
+
+  return (
+    <>
+      <div className={styles.button + " " + style} onClick={handle}>
+        {<div className="button__text">{text}</div>}
+      </div>
+    </>
+  );
+};
