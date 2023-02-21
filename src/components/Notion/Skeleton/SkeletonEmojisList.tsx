727a55c1ba1bb22762a4d3d85916aa3378be5b2d
Ivan Varabyou <85835188+ivan-varabyou@users.noreply.github.com>
2023-02-21 03:08:45 +0300

    feat: add sceleton emoji

---
+++ src/components/Notion/Skeleton/SkeletonEmojisList.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const SkeletonEmojisList: React.FC = () => (
+  <ContentLoader
+    speed={2}
+    width={330}
+    height={410}
+    viewBox="0 0 330 410"
+    backgroundColor="#e8e8e8"
+    foregroundColor="#ababab"
+  >
+    <circle cx="22" cy="19" r="15" />
+    <circle cx="204" cy="19" r="15" />
+    <circle cx="239" cy="19" r="15" />
+    <circle cx="274" cy="19" r="15" />
+    <circle cx="311" cy="19" r="15" />
+    <circle cx="166" cy="19" r="15" />
+    <circle cx="131" cy="19" r="15" />
+    <circle cx="96" cy="19" r="15" />
+    <circle cx="58" cy="19" r="15" />
+    <circle cx="21" cy="57" r="15" />
+    <circle cx="203" cy="57" r="15" />
+    <circle cx="238" cy="57" r="15" />
+    <circle cx="273" cy="57" r="15" />
+    <circle cx="310" cy="57" r="15" />
+    <circle cx="165" cy="57" r="15" />
+    <circle cx="130" cy="57" r="15" />
+    <circle cx="95" cy="57" r="15" />
+    <circle cx="57" cy="57" r="15" />
+    <circle cx="22" cy="96" r="15" />
+    <circle cx="204" cy="96" r="15" />
+    <circle cx="239" cy="96" r="15" />
+    <circle cx="274" cy="96" r="15" />
+    <circle cx="311" cy="96" r="15" />
+    <circle cx="166" cy="96" r="15" />
+    <circle cx="131" cy="96" r="15" />
+    <circle cx="96" cy="96" r="15" />
+    <circle cx="58" cy="96" r="15" />
+    <circle cx="21" cy="134" r="15" />
+    <circle cx="203" cy="134" r="15" />
+    <circle cx="238" cy="134" r="15" />
+    <circle cx="273" cy="134" r="15" />
+    <circle cx="310" cy="134" r="15" />
+    <circle cx="165" cy="134" r="15" />
+    <circle cx="130" cy="134" r="15" />
+    <circle cx="95" cy="134" r="15" />
+    <circle cx="57" cy="134" r="15" />
+    <circle cx="21" cy="172" r="15" />
+    <circle cx="203" cy="172" r="15" />
+    <circle cx="238" cy="172" r="15" />
+    <circle cx="273" cy="172" r="15" />
+    <circle cx="310" cy="172" r="15" />
+    <circle cx="165" cy="172" r="15" />
+    <circle cx="130" cy="172" r="15" />
+    <circle cx="95" cy="172" r="15" />
+    <circle cx="57" cy="172" r="15" />
+    <circle cx="20" cy="210" r="15" />
+    <circle cx="202" cy="210" r="15" />
+    <circle cx="237" cy="210" r="15" />
+    <circle cx="272" cy="210" r="15" />
+    <circle cx="309" cy="210" r="15" />
+    <circle cx="164" cy="210" r="15" />
+    <circle cx="129" cy="210" r="15" />
+    <circle cx="94" cy="210" r="15" />
+    <circle cx="56" cy="210" r="15" />
+    <circle cx="21" cy="249" r="15" />
+    <circle cx="203" cy="249" r="15" />
+    <circle cx="238" cy="249" r="15" />
+    <circle cx="273" cy="249" r="15" />
+    <circle cx="310" cy="249" r="15" />
+    <circle cx="165" cy="249" r="15" />
+    <circle cx="130" cy="249" r="15" />
+    <circle cx="95" cy="249" r="15" />
+    <circle cx="57" cy="249" r="15" />
+    <circle cx="20" cy="287" r="15" />
+    <circle cx="202" cy="287" r="15" />
+    <circle cx="237" cy="287" r="15" />
+    <circle cx="272" cy="287" r="15" />
+    <circle cx="309" cy="287" r="15" />
+    <circle cx="164" cy="287" r="15" />
+    <circle cx="129" cy="287" r="15" />
+    <circle cx="94" cy="287" r="15" />
+    <circle cx="56" cy="287" r="15" />
+    <circle cx="20" cy="326" r="15" />
+    <circle cx="202" cy="326" r="15" />
+    <circle cx="237" cy="326" r="15" />
+    <circle cx="272" cy="326" r="15" />
+    <circle cx="309" cy="326" r="15" />
+    <circle cx="164" cy="326" r="15" />
+    <circle cx="129" cy="326" r="15" />
+    <circle cx="94" cy="326" r="15" />
+    <circle cx="56" cy="326" r="15" />
+    <circle cx="21" cy="365" r="15" />
+    <circle cx="203" cy="365" r="15" />
+    <circle cx="238" cy="365" r="15" />
+    <circle cx="273" cy="365" r="15" />
+    <circle cx="310" cy="365" r="15" />
+    <circle cx="165" cy="365" r="15" />
+    <circle cx="130" cy="365" r="15" />
+    <circle cx="95" cy="365" r="15" />
+    <circle cx="57" cy="365" r="15" />
+    <circle cx="20" cy="403" r="15" />
+    <circle cx="202" cy="403" r="15" />
+    <circle cx="237" cy="403" r="15" />
+    <circle cx="272" cy="403" r="15" />
+    <circle cx="309" cy="403" r="15" />
+    <circle cx="164" cy="403" r="15" />
+    <circle cx="129" cy="403" r="15" />
+    <circle cx="94" cy="403" r="15" />
+    <circle cx="56" cy="403" r="15" />
+  </ContentLoader>
+);
+
+export default SkeletonEmojisList;
