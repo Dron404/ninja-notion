1f31cd40f3eac177a4b59d3c3509ab45c7a4b0ee
Ivan Varabyou <85835188+ivan-varabyou@users.noreply.github.com>
2023-02-21 03:32:44 +0300

    fix: add move home page

--- src/utils/update/moveToPage.ts
+++ src/utils/update/moveToPage.ts
@@ -37,10 +37,13 @@ const moveToPage = (
     });
     return iterationPages;
   }
+
   const result = nextPage(pages);
-  if (pageId === "") {
+
+  if (pageId.length > 0) {
     result.push(dataPage);
   }
+  console.log("pages", pages, "pageId", pageId, "result", result);
   return result;
 };
 
