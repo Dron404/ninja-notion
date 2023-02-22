aa22b9fb30efdd316263216be58b324cb44cd9bc
AnastasiaShalukhina <exachka4@gmail.com>
2023-02-22 12:38:54 +0300

    fix: remove lint warnings

--- src/components/Home/DropDown.tsx
+++ src/components/Home/DropDown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, SetStateAction } from "react";
+import React from "react";
 // import geo from "../../assets/images/home/geography.png";
 // import arrowDown from "../../assets/images/home/arrow-down.png";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
@@ -8,56 +8,6 @@ import { Tlanguage } from "../../types/types";
 import { Menu } from "@headlessui/react";
 import { Button } from "../Notion/buttons/Button";
 import styles from "../Notion/Settings/Language/Language.module.scss";
-
-// function DropDown(props: {
-//   selected: string;
-//   setSelected: React.Dispatch<SetStateAction<string>>;
-// }) {
-//   const { selected, setSelected } = props;
-//   const [isActive, setIsActive] = useState(false);
-//   const options: Array<string> = ["Русский", "Польский", "Английский"];
-//   return (
-//     <div className="dropdown">
-//       {isActive && (
-//         <div className="dropdown-content">
-//           {options.map((option: string) => (
-//             <div
-//               tabIndex={0}
-//               role="button"
-//               key={option}
-//               onClick={() => {
-//                 setSelected(option);
-//                 setIsActive(false);
-//               }}
-//               onKeyDown={() => {
-//                 setSelected(option);
-//                 setIsActive(false);
-//               }}
-//               className="dropdown-item"
-//             >
-//               {option}
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//       <div
-//         onKeyDown={() => {
-//           setIsActive(!isActive);
-//         }}
-//         tabIndex={0}
-//         role="button"
-//         className="dropdown-btn"
-//         onClick={() => {
-//           setIsActive(!isActive);
-//         }}
-//       >
-//         <img src={geo} alt="black-white earth" className="geography" />
-//         {selected}
-//         <img src={arrowDown} alt="arrow down" className="arrow-down" />
-//       </div>
-//     </div>
-//   );
-// }
 
 const DropDown: React.FC = () => {
   const dispatch = useAppDispatch();
